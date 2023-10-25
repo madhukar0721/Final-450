411740278cdc947e6b02cc73676faad88b87354b
Madhukar Kumar <88223881+madhukar0721@users.noreply.github.com>
2023-10-26 00:38:34 +0530

    Create 003. Find the "Kth" max and min element of an array.cpp

---
+++ 003. Find the "Kth" max and min element of an array.cpp
@@ -0,0 +1,23 @@
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+        //code here
+        
+        priority_queue<int> maxHeap;
+        
+        for(int i=0;i<k;i++)
+            maxHeap.push(arr[i]);
+        
+        for(int i =k;i<=r;i++)
+        {
+            maxHeap.push(arr[i]);
+            if(maxHeap.size()>k)
+            maxHeap.pop();
+        }
+        return maxHeap.top();
+    }
+};
